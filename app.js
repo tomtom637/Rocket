798d3ccb33ce658cacb2e2e15db5732190b31843
ToM <tomtom637@gmail.com>
2020-03-26 23:45:26 +0100

    "added SASS"

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const PORT = process.env.PORT || 3737;
 // Set pug as the View Engine
 app.set('view engine', 'pug');
 
+// Used instead of body-parser to get the values of our inputs (req.body)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// We set the directory for our static files
+app.use(express.static('assets'));
+
 // Routes
 app.use('/', require('./routes/index'));
 
